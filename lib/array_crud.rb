ecb64b29a5c06fb6e30eb96ce3d1d4e060eb159b
Jamal Gelin <gelin.jamal@gmail.com>
2018-03-12 03:46:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["wow", "one", "two"]
-  +  array.shift
+array = ["1","2"]
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
